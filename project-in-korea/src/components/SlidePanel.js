c557d150c2921758b111ae2b7ec47a69783a5cac
graykick <graykick@naver.com>
2017-07-05 21:50:28 +0900

    add react router
    add react router

--- project-in-korea/src/components/SlidePanel.js
+++ project-in-korea/src/components/SlidePanel.js
@@ -1,19 +1,22 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from '../res/logo.svg';
 import '../res/App.css';
 import image1 from '../res/images/slide1.jpg';
 import image2 from '../res/images/slide2.jpeg';
 import image3 from '../res/images/slide3.jpg';
-
 class SlidePanel extends Component {
-  render() {
-    return (
-      <div className="slide-panel">
-          <img src={image1} alt="slide image" className="slide-image"/>
-          <img src={image2} alt="slide image" className="slide-image"/>
-          <img src={image3} alt="slide image" className="slide-image"/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="slide-panel">
+                <table>
+                    <tr>
+                        <td><img src={image1} alt="slide image" className="slide-image"/></td>
+                        <td><img src={image2} alt="slide image" className="slide-image"/></td>
+                        <td><img src={image3} alt="slide image" className="slide-image"/></td>
+                    </tr>
+                </table>
+            </div>
+        );
+    }
 }
 export default SlidePanel;
--- project-in-korea/src/containers/App.js
+++ project-in-korea/src/containers/App.js
@@ -1,46 +1,20 @@
 import React, { Component } from 'react';
-import logo from '../res/logo.svg';
-import '../res/App.css';
-import Header from './Header';
-import Nav from './Nav';
-import SlidePanel from '../components/SlidePanel';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-
+import Home from '../routes/Home';
+import Login from '../routes/Login';
+import Register from '../routes/Register';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="root-container">
-            <Header/>
-            <Nav/>
-            <SlidePanel/>
-            <div className="search-panel">
-                <table>
-                    <tr>
-                        <td> <button type="button" name="button" class="specific-search-button">세부검색</button>
-                        </td>
-                        <td> <input type="text" name="search" value="" placeholder="검색어를 입력하세요"/>
-                        </td>
-                        <td> <button type="button" name="button">검색</button>
-                        </td>
-                        <td> <button type="button" name="button">?</button>
-                        </td>
-                        <td> <button type="button" name="button">?</button>
-                        </td>
-                        <td> <button type="button" name="button">???</button>
-                        </td>
-                    </tr>
-                </table>
-            </div>
-            <div className="specific-search-panel">
-                상세검색 미정
-            </div>
-            <div className="content-panel">
-
-            </div>
+      <Router>
+        <div className="App">
+            <Route exact path="/" component={Home}></Route>
+            <Route exact path="/Login" component={Login}></Route>
+            <Route exact path="/Register" component={Register}></Route>
         </div>
-      </div>
+      </Router>
     );
   }
 }
